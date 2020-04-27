40438552dfb1e32c35a83f5578eded922ee16cd0
yangjiawei <1448348536@qq.com>
2020-04-28 07:57:48 +0800

    初始化左侧菜单

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Menu } from 'antd';
+import menuConfig from '../../config/menuConfig';
 
 import './index.less';
 
+let {SubMenu} = Menu;
+
 export default class NavLeft extends React.Component{
+    componentWillMount(){
+        let menuItem = this.menuRender(menuConfig);
+        this.setState({
+            menuItem
+        })
+    }
+    menuRender = (data) =>{
+        return data.map((item) => {
+            if(item.children){
+                return (<SubMenu key={item.key} title={item.title}>
+                    {this.menuRender(item.children)}
+                </SubMenu>)
+            }
+            return (<Menu.Item key={item.key}>{item.title}</Menu.Item>)
+        })
+    }
     render(){
-        return (<div>
+        return (<div style={{height:'100vh',backgroundColor:'#001529'}}>
             <div className="logo">
                 <img className="icon" src="/assets/logo-ant.svg" alt=""/>
                 <h1>Imooc MS</h1>
             </div>
-            <Menu theme="dark">
-                <Menu.Item>Option 5</Menu.Item>
-                <Menu.Item>Option 6</Menu.Item>
-            </Menu>
+            <Menu theme="dark">{this.state.menuItem}</Menu>
         </div>);
     }
 }
